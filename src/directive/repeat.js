ee986e8a73d81a759313dbc4b769d98ee17fc413
Air <baishuiz@gmail.com>
2015-10-08 15:44:43 +0800

    update

--- src/directive/repeat.js
+++ src/directive/repeat.js
@@ -64,7 +64,7 @@ Air.Module("directive.repeat", function(require){
 
 								beacon({target:newNode, scope:$scope}).once("cloneNodeRemove", function(e, $scope){
 									if($scope !== this.scope) return;
-                  this.parentElement.removeChild(this);
+                  this.target.parentElement.removeChild(this);
 								});
 
                 newNode.removeAttribute(key);
