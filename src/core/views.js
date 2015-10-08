5953dc9c260862938bf5f9063324fdbcd46d11bc
Air <baishuiz@gmail.com>
2015-10-08 12:32:03 +0800

    fix view.init

--- src/core/views.js
+++ src/core/views.js
@@ -9,7 +9,7 @@ Air.Module('core.views', function(require){
     router : router,
     count:0,
     init : function(urlPath){
-      urlPath = urlPath || window.location;
+      urlPath = urlPath || window.location.pathName;
       var target = document.querySelector("viewport[main='true'] view[active='true']");
       if(!target){
         api.count = 0
