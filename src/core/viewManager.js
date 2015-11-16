0a0b3a540b7af6296dd1fa66db329fbf9db272b8
Mengfei Xue <xuemengfei@vip.qq.com>
2015-11-16 18:14:02 +0800

    noRender param

--- src/core/viewManager.js
+++ src/core/viewManager.js
@@ -1,23 +1,23 @@
 Air.Module('core.viewManager', function(require){
-	var config            = require('core.config'),
-	    scopeList         = require('core.scopeList'),
-	    generateScopeTree = require("core.scopeTree"),
-	    url               = require('core.url');
-	var viewList   = {};
-	var viewStatus = {
-		active : null,
-		count  : 0
-	}
+    var config            = require('core.config'),
+        scopeList         = require('core.scopeList'),
+        generateScopeTree = require("core.scopeTree"),
+        url               = require('core.url');
+    var viewList   = {};
+    var viewStatus = {
+        active : null,
+        count  : 0
+    }
 
-	function parseResource (template){
-		var content = template.replace(/(href|src)=([\"])([^'"]+)\2/ig, function(src){
-		    var result = src.replace("{{resourceUrl}}", config.get("resourceUrl") || "");
-		    return result
-		});
-		return content;
-	}
+    function parseResource (template){
+        var content = template.replace(/(href|src)=([\"])([^'"]+)\2/ig, function(src){
+            var result = src.replace("{{resourceUrl}}", config.get("resourceUrl") || "");
+            return result
+        });
+        return content;
+    }
 
-	function loadController(view){
+    function loadController(view){
         // load controller
         var scripts = view.querySelectorAll('script');
         scripts = [].slice.call(scripts);
@@ -34,9 +34,9 @@ Air.Module('core.viewManager', function(require){
 
           activeScript.parentNode.removeChild(activeScript);
         };
-	}
+    }
 
-	function append(viewName, template, options){
+    function append(viewName, template, options){
         var viewport = document.querySelector("viewport[main='true']");
         var view     = document.createElement("view");
         var template = parseResource(template);
@@ -50,99 +50,101 @@ Air.Module('core.viewManager', function(require){
         viewport.appendChild(view);
         viewStatus.count += 1;
         viewStatus.active = view;
-		scopeList.init(view, generateScopeTree);
+        scopeList.init(view, generateScopeTree);
         loadController(view);
-	}
+    }
 
-	function regist(viewName, view, template, options){
+    function regist(viewName, view, template, options){
         options = options || {};
         viewList[viewName] = {
-        	template : template,
-        	view     : view,
-        	url      : url.getURLPath(viewName, options)
+            template : template,
+            view     : view,
+            url      : url.getURLPath(viewName, options)
         }
-	}
+    }
 
-	function init(){
-		var views  = document.querySelectorAll("viewport[main='true'] view");
-		for (var viewIndex = 0; viewIndex < views.length; viewIndex++) {
-			var view = views[viewIndex];
-			var viewName = view.getAttribute('name') || 'view' + viewIndex;
-			var template = view.innerHTML;
-			regist(viewName, view, template);
-		};
+    function init(){
+        var views  = document.querySelectorAll("viewport[main='true'] view");
+        for (var viewIndex = 0; viewIndex < views.length; viewIndex++) {
+            var view = views[viewIndex];
+            var viewName = view.getAttribute('name') || 'view' + viewIndex;
+            var template = view.innerHTML;
+            regist(viewName, view, template);
+        };
 
-      	viewStatus.count  = views.length;
-      	viewStatus.active = document.querySelector("viewport[main='true'] view[active='true']");
-	}
+          viewStatus.count  = views.length;
+          viewStatus.active = document.querySelector("viewport[main='true'] view[active='true']");
+    }
 
-	function setActive(target){
-		viewStatus.active && viewStatus.active.removeAttribute('active');
-		target.setAttribute('active','true');
-		viewStatus.active = target;
-	}
+    function setActive(target){
+        viewStatus.active && viewStatus.active.removeAttribute('active');
+        target.setAttribute('active','true');
+        viewStatus.active = target;
+    }
 
-	function show(viewName, options){
+    function show(viewName, options){
         var urlPath = url.getURLPath(viewName, options);
         var targetView = viewList[viewName];
         // return targetView;
         if (targetView && targetView.url === urlPath){
-        	setActive(targetView.view);
+            setActive(targetView.view);
             return targetView;
         } else if(targetView && targetView.url !== urlPath){
-            removeSingle(viewName);
-            append(viewName, targetView.template, options);
+            if (!options.noRender) {
+                removeSingle(viewName);
+                append(viewName, targetView.template, options);
+            }
             return targetView;
         }
 
-	}
+    }
 
 
 
     function removeAll(){
         for(var viewName in viewList){
-        	removeSingle(viewName)
+            removeSingle(viewName)
         }
         viewStatus.active = null;
         viewStatus.count  = 0;
     }
 
     function removeSingle(viewName){
-    	var view =  viewList[viewName].view;
-    	view.parentNode.removeChild(view);
-    	delete viewList[viewName];
-    	viewStatus.count -= 1;
-        if(	view.getAttribute("active")==="true"){
-        	viewStatus.active = null;
+        var view =  viewList[viewName].view;
+        view.parentNode.removeChild(view);
+        delete viewList[viewName];
+        viewStatus.count -= 1;
+        if(    view.getAttribute("active")==="true"){
+            viewStatus.active = null;
         }
     }
 
 
 
     Air.domReady(function(){
-    	init();
+        init();
     });
 
-	var api = {
-		append : append,
-		show   : show,
-		getCount : function(num){
-			viewStatus.count = num || viewStatus.count;
-			return viewStatus.count
-		},
+    var api = {
+        append : append,
+        show   : show,
+        getCount : function(num){
+            viewStatus.count = num || viewStatus.count;
+            return viewStatus.count
+        },
 
-		getActive : function(){
-			return viewStatus.active
-		},
+        getActive : function(){
+            return viewStatus.active
+        },
 
-		remove : function(viewName){
-			var view = viewList[viewName];
+        remove : function(viewName){
+            var view = viewList[viewName];
             viewName ? removeSingle(viewName) : removeAll()
-		}
+        }
 
 
-	}
+    }
 
 
-	return api;
+    return api;
 });
