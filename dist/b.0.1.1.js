c9f39eef64b072378593cb09d3d065ab8f0223a1
Mengfei Xue <xuemengfei@vip.qq.com>
2016-01-04 10:57:27 +0800

    add config for switchStyle

--- dist/b.0.1.1.js
+++ dist/b.0.1.1.js
@@ -146,13 +146,14 @@
     return query;
 });
 ;Air.Module('utility.switchStyle', function(require) {
+    var config = require('core.config');
     var switchStyle = (function(){
-        var css = '[ng-app] { text-indent: -10000%; background-color: #eee; }',
-        head = document.head || document.getElementsByTagName('head')[0],
+        var head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
 
         style.type = 'text/css';
         var appendStyle = function() {
+          var css = config.get('switchStyle') || '[ng-app] { text-indent: -10000%; background-color: #eee; }';
           if (style.styleSheet){
             style.styleSheet.cssText = css;
           } else {
