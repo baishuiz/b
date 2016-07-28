f61267b6ab3472ba4779fc2ae3902832af2f72c9
Air <baishuiz@gmail.com>
2016-07-28 19:46:38 +0800

    remove todo

--- src/B/scope/scopeManager.js
+++ src/B/scope/scopeManager.js
@@ -130,7 +130,6 @@ Air.Module('B.scope.scopeManager', function(require) {
 
   /**
    *作用：解析文本|属性节点，监听数据变化
-   * TODO option
    *参数: <node> 文本节点|属性节点
    *参数: <currentScopeIndex> 数据标签所在作用域索引值
    *返回：undefind
