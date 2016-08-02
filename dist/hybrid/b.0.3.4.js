d28d3d2952d69bd3e15b551519f41eaece9d338a
Mengfei Xue <xuemengfei@vip.qq.com>
2016-08-02 20:13:08 +0800

    add HTTP to b.utility

--- dist/hybrid/b.0.3.4.js
+++ dist/hybrid/b.0.3.4.js
@@ -2870,8 +2870,9 @@ Air.run(function(require){
       router = require("B.router.router"),
       memCache = require('B.data.memCache'),
       run = require('B.controller.run'),
-      serviceFactory = require('B.service.serviceFactory');
-      TDK = require('B.TDK.TDK');
+      serviceFactory = require('B.service.serviceFactory'),
+      HTTP = require('B.network.HTTP'),
+      TDK = require('B.TDK.TDK'),
       bridge = require('B.bridge');
   void function main(){
     var FRAMEWORK_NAME = "b";
@@ -2879,7 +2880,9 @@ Air.run(function(require){
       views    : viewManager, // ViewManager
       router   : router, // Router
       service  : serviceFactory,
-      utility  : null,
+      utility  : {
+        HTTP: HTTP
+      },
 
       /**
        * [环境初始化]
--- dist/web/b.0.3.4.js
+++ dist/web/b.0.3.4.js
@@ -2680,7 +2680,8 @@ Air.run(function(require){
       router = require("B.router.router"),
       memCache = require('B.data.memCache'),
       run = require('B.controller.run'),
-      serviceFactory = require('B.service.serviceFactory');
+      serviceFactory = require('B.service.serviceFactory'),
+      HTTP = require('B.network.HTTP'),
       TDK = require('B.TDK.TDK');
   void function main(){
     var FRAMEWORK_NAME = "b";
@@ -2688,7 +2689,9 @@ Air.run(function(require){
       views    : viewManager, // ViewManager
       router   : router, // Router
       service  : serviceFactory,
-      utility  : null,
+      utility  : {
+        HTTP: HTTP
+      },
 
       /**
        * [环境初始化]
