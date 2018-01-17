2c089053d1b0ca0b4bc7ab610766b79325154291
霍超群 <huochaoqun@cjia.com>
2018-01-17 18:15:24 +0800

    恢复测试代码

--- test/spec/repeat/repeat.with.B-exist.Spec.js
+++ test/spec/repeat/repeat.with.B-exist.Spec.js
@@ -62,49 +62,41 @@ describe('repeat with B-exist', function () {
     });
   });
 
-  it('subRepeat has b-exist', function (done) {
-    b.run('page_repeat_subRepeat_has_b-exist', function(require, $scope){
-      var list = [
-        {name: 'T1', status: 1, div1: [{status: false, name: 'div1_name1'}]},
-        {name: 'T2', status: 0, div1: [{status: false, name: 'div1_name2'}]},
-        {name: 'T2', status: 0, div1: [{status: true, name: 'div1_name2'}]},
-        {name: 'T3', status: 2, div1: [{status: false, name: 'div1_name3'}]}
-      ]
+    it('repeat sub element with repeat', function (done) {
+        b.run('page_repeat_subElement_with_b-repeat', function(require, $scope){
+            var list = [
+                {name: 'T1', status: 1, div1: [{status: false, name: 'div1_name1'}]},
+                {name: 'T2', status: 0, div1: [{status: false, name: 'div1_name2'}]},
+                {name: 'T2', status: 0, div1: [{status: true, name: 'div1_name2'}]},
+                {name: 'T3', status: 2, div1: [{status: false, name: 'div1_name3'}]}
+            ]
 
-      // list.forEach(function (item) {
-      //   item.showStatus_1 = !(item.status === 1 && !item.div1[0].status);
-      //   item.showStatus_2 = !(item.status === 0 && item.div1[0].status);
-      // });
+            list.forEach(function (item) {
+                item.showStatus_1 = !(item.status === 1 && !item.div1[0].status);
+                item.showStatus_2 = !(item.status === 0 && item.div1[0].status);
+            });
 
-      $scope.list = list;
+            $scope.list = list;
 
-      setTimeout(function(){
-        var dom = {
-          listC: document.querySelectorAll('view[name=page_repeat_subRepeat_has_b-exist]>.listC'),
-          spanParent: document.querySelectorAll('view[name=page_repeat_subRepeat_has_b-exist] .spanParent'),
-          btnsParent: document.querySelectorAll('view[name=page_repeat_subRepeat_has_b-exist] .btnsParent')
-        }
-
-
-        expect(dom.listC.length).toEqual(4);
-        expect(dom.spanParent.length).toEqual(8);
-        expect(dom.btnsParent.length).toEqual(8);
+            setTimeout(function(){
+                var dom = {
+                    listC: document.querySelectorAll('view[name=page_repeat_subElement_with_b-repeat]>.listC'),
+                    spanParent: document.querySelectorAll('view[name=page_repeat_subElement_with_b-repeat] .spanParent'),
+                    btnsParent: document.querySelectorAll('view[name=page_repeat_subElement_with_b-repeat] .btnsParent')
+                }
 
 
-        expect(dom.spanParent[0].querySelector('.span').innerText.trim()).toEqual('T1');
-        expect(dom.spanParent[0].querySelectorAll('button')[0].innerText.trim()).toEqual('div1_name1');
+                // expect(dom.listB.length).toEqual(1);
+                expect(dom.spanParent.length).toEqual(2);
+                expect(dom.spanParent[0].querySelector('.span').innerText.trim()).toEqual('T1');
+                // expect(dom.btnsParent.length).toEqual(4);
+                // expect(dom.btnsParent[1].querySelectorAll('.btns').length).toEqual(2);
+                // expect(dom.btnsParent[1].querySelectorAll('.btns button').length).toEqual(1);
+                // expect(dom.btnsParent[1].querySelectorAll('.btns button')[0].innerText.trim()).toEqual('div1_name2');
 
-        expect(dom.listC[0].querySelectorAll('.spanParent')[1].querySelectorAll('button')[0].innerText.trim()).toEqual('div1_name1');
+                done();
 
-        // expect(dom.spanParent[0].querySelector('.span').innerText.trim()).toEqual('T1');
-        // expect(dom.btnsParent.length).toEqual(4);
-        // expect(dom.btnsParent[1].querySelectorAll('.btns').length).toEqual(2);
-        // expect(dom.btnsParent[1].querySelectorAll('.btns button').length).toEqual(1);
-        // expect(dom.btnsParent[1].querySelectorAll('.btns button')[0].innerText.trim()).toEqual('div1_name2');
-
-        done();
-
-      }, 500);
+            }, 500);
+        });
     });
-  });
 });
